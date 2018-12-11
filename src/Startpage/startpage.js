069aed04d60f050524ce85e951a8477aa4cbb0d0
tasola <petter.tk@hotmail.com>
2018-12-11 17:15:23 +0100

    Add start of a startpage

--- src/Startpage/startpage.js
+++ src/Startpage/startpage.js
@@ -5,7 +5,12 @@ class Startpage extends Component {
   render() {
     return (
       <div className="startPage">
-        <h2>Startpage</h2>
+        <div className="headerWrapper">
+          <h2>VRStressed</h2>
+        </div>
+        <div className="scrollWrapper">
+          <a className="icon-scroll" href="#about" />
+        </div>
       </div>
     );
   }
