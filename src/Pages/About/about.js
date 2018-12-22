552f49f7e17dee9b6126dc10713ea3d0c526cae0
tasola <petter.tk@hotmail.com>
2018-12-22 18:25:32 +0100

    Add start of parallax about page

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -5,10 +5,18 @@ class About extends Component {
   render() {
     return (
       <div className="aboutPage">
-        <h2>About</h2>
+        <div className="aboutImgWrapper">
+          <div className="aboutImg" />
+        </div>
+        <div className="aboutContentWrapper">
+          <div className="aboutContent">
+            <h2>About</h2>
+            <p>Lorem ipsum</p>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
+//Users/Petter/documents/github/agi-p2-web/src/resources/images/about/about.jpg
 export default About;
