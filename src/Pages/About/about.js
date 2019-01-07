315bfeab7a549095a13ecbce2ebb860b3f3c75b9
tasola <petter.tk@hotmail.com>
2019-01-07 21:36:20 +0100

    Fix merge conflicts

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import { Parallax } from 'react-parallax';
 import './about.css';
 import ScrollableAnchor from 'react-scrollable-anchor';
+
+const aboutImg = 'https://s3-eu-west-1.amazonaws.com/grm-assets/18088/maxresdefault.jpg';
 
 class About extends Component {
   render() {
     return (
-      <ScrollableAnchor id="aboutPage">
-        <div className="aboutPageWrapper">
-          <h2>About</h2>
-        </div>
-      </ScrollableAnchor>
+      <div className="aboutPage">
+        <ScrollableAnchor id="aboutPage">
+        <Parallax className="aboutParallax" bgImage={aboutImg} strength={600}>
+          <div className="aboutContentWrapper">
+            <div className="aboutContent">
+              <h2>About</h2>
+              <p>Lorem ipsum</p>
+            </div>
+          </div>
+        </Parallax>
+        </ScrollableAnchor>
+      </div>
     );
   }
 }
-//Users/Petter/documents/github/agi-p2-web/src/resources/images/about/about.jpg
+
 export default About;
+
+// import React, { Component } from 'react';
+// import './about.css';
+//
+// const aboutImg = 'https://s3-eu-west-1.amazonaws.com/grm-assets/18088/maxresdefault.jpg';
+//
+// class About extends Component {
+//   render() {
+//     return (
+//       <div className="aboutPage">
+//         <div className="aboutImgWrapper">
+//           <div className="aboutImg" />
+//         </div>
+//         <div className="aboutContentWrapper">
+//           <div className="aboutContent">
+//             <h2>About</h2>
+//             <p>Lorem ipsum</p>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// //Users/Petter/documents/github/agi-p2-web/src/resources/images/about/about.jpg
+// export default About;
