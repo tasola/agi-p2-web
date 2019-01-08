3dc7eef50c16cd9aa1e090c4dc126372ed7ee8aa
gustjoh <gustjoh@kth.se>
2019-01-08 23:50:50 +0100

    Restyled text a bit, changed to all small characters and position

--- src/Pages/Team/team.js
+++ src/Pages/Team/team.js
@@ -18,22 +18,22 @@ class Team extends Component {
     super(props);
     this.state = {
       selectedProfile: {
-        id: 2,
-        firstname: 'Linnea',
-        lastname: 'Holm',
-        role: 'WEB DEVELOPER / UX / COPY',
+        id: 1,
+        firstname: 'julian',
+        lastname: 'hedén',
+        role: 'game developer',
         description:
-          "Responsible for the user experience of the game, Linnea has taken lead on the UX research, testing and evaluation during the development of this game. She has also been involved in some of the visual design decisions throughout the development process. At the later stages of the project, she was part of the website developing team, equally focused on coding and styling as well as being the project's copywriter.",
+          "Julian's contributions mostly consist of Augmented Reality and game programming within the app. His focus has been on creating, localizing and keeping track of objects in the AR space. He's also developed some game mechanics, such as the wind and boundaries/fences. Julian has also acted as part project manager and helped merge the pieces together (being the only one with an ARCore compatible device).",
         linkedin: 'https://www.linkedin.com/in/gustav-johansson-b85b42109/',
         github: 'https://github.com/gustavjohansson',
-        image: ImageLinnea,
+        image: ImageJulian,
       },
       profiles: [
         {
           id: 1,
-          firstname: 'Julian',
-          lastname: 'Hedén',
-          role: 'AR-DEVELOPER',
+          firstname: 'julian',
+          lastname: 'hedén',
+          role: 'game developer',
           description:
             "Julian's contributions mostly consist of Augmented Reality and game programming within the app. His focus has been on creating, localizing and keeping track of objects in the AR space. He's also developed some game mechanics, such as the wind and boundaries/fences. Julian has also acted as part project manager and helped merge the pieces together (being the only one with an ARCore compatible device).",
           linkedin: 'https://www.linkedin.com/in/gustav-johansson-b85b42109/',
@@ -42,9 +42,9 @@ class Team extends Component {
         },
         {
           id: 2,
-          firstname: 'Linnea',
-          lastname: 'Holm',
-          role: 'WEB DEVELOPER / UX / COPY',
+          firstname: 'linnea',
+          lastname: 'holm',
+          role: 'ux designer',
           description:
             "Responsible for the user experience of the game, Linnea has taken lead on the UX research, testing and evaluation during the development of this game. She has also been involved in some of the visual design decisions throughout the development process. At the later stages of the project, she was part of the website developing team, equally focused on coding and styling as well as being the project's copywriter.",
           linkedin: 'https://www.linkedin.com/in/gustav-johansson-b85b42109/',
@@ -53,9 +53,9 @@ class Team extends Component {
         },
         {
           id: 3,
-          firstname: 'Gustav',
-          lastname: 'Johansson',
-          role: 'GAME DEVELOPER',
+          firstname: 'gustav',
+          lastname: 'johansson',
+          role: 'web developer',
           description:
             'Gustav’s focus in this project have mainly been on game mechanics; animating collectibles, developing functionality for power ups, integrating modalities such as vibration on collision and moving the zeppelin when blowing on the screen, signal processing, creating the leaderboard - both programmatically but also the UI design. He has also been rewriting parts of the code to make it more object oriented and scalable for future use.',
           linkedin: 'https://www.linkedin.com/in/gustav-johansson-b85b42109/',
@@ -64,9 +64,9 @@ class Team extends Component {
         },
         {
           id: 4,
-          firstname: 'Alexandra',
-          lastname: 'Runhem',
-          role: 'AR AND WEB DEVELOPER',
+          firstname: 'alexandra',
+          lastname: 'runhem',
+          role: 'game developer',
           description:
             "Alexandra has been responsible for programming game mechanics in C#, such as physical attributes of the objects, spawning logic as well as visual feedback. Been Julian's co-programmer and brainstormed the different mathematical functions that our application is built upon. As part of the web development team, she set up the core of this Angular website as well as basic layouting and styling.",
           linkedin: 'https://www.linkedin.com/in/gustav-johansson-b85b42109/',
@@ -75,9 +75,9 @@ class Team extends Component {
         },
         {
           id: 5,
-          firstname: 'Leif',
-          lastname: 'Thysell Sundqvist',
-          role: 'ARTIST',
+          firstname: 'leif',
+          lastname: 'thysell sundqvist',
+          role: 'game developer and artist',
           description:
             'Mainly responsible for 2D and 3D design and assets as well as illustrations and animations. The 3D models were modelled in Blender and directly exported to the project in Unity. The 2D assets were designed in Adobe Photoshop, and the animated videos in Blender as well as Adobe After Effects. I also assisted with game mechanics and functionality whenever there was need of an extra hand.',
           linkedin: 'https://www.linkedin.com/in/gustav-johansson-b85b42109/',
@@ -86,9 +86,9 @@ class Team extends Component {
         },
         {
           id: 6,
-          firstname: 'Petter',
-          lastname: 'Tasola',
-          role: 'GAME AND WEB DEVELOPER',
+          firstname: 'petter',
+          lastname: 'tasola',
+          role: 'web developer',
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed turpis nulla, dapibus non vehicula ut, consequat sit amet ipsum. Nullam ut tortor condimentum, hendrerit purus hendrerit, pretium ipsum. Suspendisse ultricies, ipsum ac volutpat imperdiet, ex nulla bibendum magna, sed condimentum felis turpis et velit. Ut eleifend risus a eros interdum feugiat. Nunc rhoncus quam vitae ipsum fringilla gravida. Praesent a volutpat neque. Vestibulum vitae consectetur sapien. Aliquam accumsan quis eros sed faucibus. Phasellus sodales facilisis metus vitae mattis.',
           linkedin: 'https://www.linkedin.com/in/petter-tasola-699a8714b/',
