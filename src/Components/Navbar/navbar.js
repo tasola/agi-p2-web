77596f5e04f423fbbc45ac8f31dee683a7ab3b8a
tasola <petter.tk@hotmail.com>
2019-01-09 11:41:12 +0100

    Add navbar timer functionality

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,29 +4,71 @@ import './navbar.css';
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { isHide: true };
+    this.state = { isHide: true, time: 0, hover: false };
+    this.hoverNavBar = this.hoverNavBar.bind(this);
+    this.mouseOutNavBar = this.mouseOutNavBar.bind(this);
     this.hideNavBar = this.hideNavBar.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
   }
+
+  hoverNavBar = () => {
+    this.setState({
+      hover: true,
+    });
+  };
+
+  mouseOutNavBar = () => {
+    this.setState({
+      hover: false,
+    });
+  };
 
   hideNavBar = () => {
     let { isHide } = this.state;
     window.scrollY > this.prev ? !isHide && this.setState({ isHide: true }) : isHide && this.setState({ isHide: false });
+    this.stopTimer();
+    if (!this.state.isHide) this.startTimer();
     this.prev = window.scrollY;
   };
 
+  startTimer = () => {
+    clearInterval(this.timer);
+    this.timer = setInterval(
+      () =>
+        this.setState({
+          time: this.state.time + 1,
+        }),
+      1000,
+    );
+  };
+
+  stopTimer = () => {
+    this.setState({
+      time: 0,
+    });
+  };
+
   componentDidMount() {
+    const navbar = document.getElementById('navbar');
+    console.log(navbar);
+    navbar.addEventListener('mouseover', this.hoverNavBar);
+    navbar.addEventListener('mouseout', this.mouseOutNavBar);
     window.addEventListener('scroll', this.hideNavBar);
   }
 
   componentWillUnmount() {
+    const navbar = document.getElementById('navbar');
     window.removeEventListener('scroll', this.hideNavBar);
+    navbar.removeEventListener('mouseover', this.hoverNavBar(true));
+    navbar.removeEventListener('mouseout', this.hoverNavBar(false));
   }
 
   render() {
     let classHide = this.state.isHide ? 'hide' : '';
-    console.log(classHide);
+    if (this.state.time >= 2 && !this.state.hover) classHide = 'hide';
     return (
-      <div className={`navbar ${classHide}`}>
+      <div id="navbar" className={`navbar ${classHide}`}>
         <a href="#aboutPage">hej</a>
         <a href="#teamPage">hejdå</a>
       </div>
