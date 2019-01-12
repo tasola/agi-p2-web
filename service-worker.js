0595f054f178e048894f0b92cfbd47cdc937c4d0
tasola <petter.tk@hotmail.com>
2019-01-12 18:57:43 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/agi-p2-web/precache-manifest.183a07ba800da78336e18c4741499de9.js"
+  "/agi-p2-web/precache-manifest.b9081b89638db844f346b736d29f5023.js"
 );
 
 workbox.clientsClaim();
